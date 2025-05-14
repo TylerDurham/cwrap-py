4fa49c3932dcde0c357b5f7870b2516dbba37966
TylerDurham <TylerDurham@users.noreply.github.com>
2025-05-13 20:28:35 -0500

    Next commit.

--- cwrap/cli.py
+++ cwrap/cli.py
@@ -5,6 +5,9 @@ from core import do_figlet
 
 
 def opt_font():
+    """
+    Get the settings for the font option.
+    """
     return typer.Option(
         help="The figlet font to use.", autocompletion=opt_font_completion
     )
@@ -20,10 +23,29 @@ def opt_font_completion() -> []:
     return ["cybermedium", "cyberlarge", "cybersmall"]
 
 
+def opt_language():
+    """
+    Get the settings for the language option.
+    """
+    return typer.Option(
+        help="The language for the comments.", autocompletion=opt_language_completion
+    )
+
+
+def opt_language_completion():
+    """
+    Get a list of programming languages for completion.
+
+    Returns:
+        A list of programming languages.
+    """
+    return ["python", "javascript", "typescript", "go", "golang", "lua"]
+
+
 def main(
     text: str,
     font: Annotated[str, opt_font()] = "cybermedium",
-    language: Annotated[str] = "python",
+    language: Annotated[str, opt_language()] = "python",
 ):
 
     output = do_figlet(text, font, language)
--- cwrap/core.py
+++ cwrap/core.py
@@ -1,6 +1,18 @@
 from pyfiglet import Figlet
 
+languages = {"javascript": "//", "js": "//", "lua": "--", "python": "#"}
+
+
+def get_comment_char(lang):
+    return languages.get(lang)
+
 
 def do_figlet(text: str, font: str, language):
+    comment_char = get_comment_char(language)
+    if comment_char == None:
+        raise KeyError(f"Language {language} not supported")
+
     fig = Figlet(font=font)
-    return fig.renderText(text)
+    lines = fig.renderText(text).splitlines()
+    for line in lines:
+        print(f"{comment_char} {line}")
